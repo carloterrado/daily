1463f630609064112abbe79e63a96ce9cd9cabde
carloterrado <iamterradocarlo@gmail.com>
2023-04-01 13:53:23 +0800

    start with layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,42 @@
 
 function App() {
   return (
-    <div className="grid place-items-center h-screen">
-      <h1 className="text-7xl">Hello world!</h1>
+    <div className="flex flex-wrap gap-2 p-2">
+      <div className="min-w-[30%] h-60 flex-auto border grid place-items-center overflow-hidden relative">
+        <img src="https://images.pexels.com/photos/13064556/pexels-photo-13064556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="shop" className="absolute inset-0 object-cover w-full h-full -z-10" />
+        <div className="bg-white/60 hover:bg-white/80 px-4 py-2 text-center cursor-pointer">
+          <h2>SHOP</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="min-w-[30%] h-60 flex-auto border grid place-items-center overflow-hidden relative">
+        <img src="https://images.pexels.com/photos/13064556/pexels-photo-13064556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="shop" className="absolute inset-0 object-cover w-full h-full -z-10" />
+        <div className="bg-white/60 hover:bg-white/80 px-4 py-2 text-center cursor-pointer">
+          <h2>SHOP</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="min-w-[30%] h-60 flex-auto border grid place-items-center overflow-hidden relative">
+        <img src="https://images.pexels.com/photos/13064556/pexels-photo-13064556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="shop" className="absolute inset-0 object-cover w-full h-full -z-10" />
+        <div className="bg-white/60 hover:bg-white/80 px-4 py-2 text-center cursor-pointer">
+          <h2>SHOP</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="min-w-[30%] h-60 flex-auto border grid place-items-center overflow-hidden relative">
+        <img src="https://images.pexels.com/photos/13064556/pexels-photo-13064556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="shop" className="absolute inset-0 object-cover w-full h-full -z-10" />
+        <div className="bg-white/60 hover:bg-white/80 px-4 py-2 text-center cursor-pointer">
+          <h2>SHOP</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="min-w-[30%] h-60 flex-auto border grid place-items-center overflow-hidden relative">
+        <img src="https://images.pexels.com/photos/13064556/pexels-photo-13064556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="shop" className="absolute inset-0 object-cover w-full h-full -z-10" />
+        <div className="bg-white/60 hover:bg-white/80 px-4 py-2 text-center cursor-pointer">
+          <h2>SHOP</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
