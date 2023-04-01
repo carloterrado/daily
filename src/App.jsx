f6e699c6370457a00db2dc5911db39d1f620d301
carloterrado <iamterradocarlo@gmail.com>
2023-04-01 13:28:45 +0800

    add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <h1 className="text-7xl">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
